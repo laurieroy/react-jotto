34b0197db87e7c72cbc3ee6c3ead781f93f6bd0a
lauretteroy <laurieroy.dev@gmail.com>
2022-02-12 14:41:01 -0800

    Add non-zero guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -25,23 +25,52 @@ it("does not throw warning with expected props", () => {
 });
 
 describe("if there are no words guessed", () => {
-	let wrapper
+  let wrapper;
 
-	beforeEach(() => {
-		wrapper = setup({ guessedWords: [] });
-	})
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
 
   it("renders without error", () => {
-		const component = findyByTestAttr(wrapper, "component-guessed-words")
+    const component = findyByTestAttr(wrapper, "component-guessed-words");
 
-		expect(component.length).toBe(1)
+    expect(component.length).toBe(1);
   });
 
   it("renders instructions to guess a word", () => {
-		const instructions = findyByTestAttr(wrapper, "guess-instructions")
+    const instructions = findyByTestAttr(wrapper, "guess-instructions");
 
-		expect(instructions.text().length).not.toBe(0)
-	});
+    expect(instructions.text().length).not.toBe(0);
+  });
 });
 
-describe("if there are words guessed", () => {});
+describe("if there are words guessed", () => {
+  let wrapper;
+
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  it("renders without error", () => {
+    const component = findyByTestAttr(wrapper, "component-guessed-words");
+
+    expect(component.length).toBe(1);
+  });
+
+  it("renders guessed words section", () => {
+    const guessedWordsNode = findyByTestAttr(wrapper, "guessed-words");
+
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  it("renders correct number of guessed words", () => {
+    const guessedWordsNodes = findyByTestAttr(wrapper, "guessed-word");
+
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
+});
