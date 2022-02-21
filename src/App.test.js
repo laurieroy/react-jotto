8b5297d74b2a2f0e694fbdd4927a578a408ca6f8
lauretteroy <laurieroy.dev@gmail.com>
2022-02-20 16:51:41 -0800

    Basis for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from "enzyme";
+import App from "./App";
 
-xtest('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { findyByTestAttr } from "./test/testUtils";
+
+/**
+ * Setup function for App component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+};
+
+test("renders without error", () => {
+  const wrapper = setup();
+  const appComponent = findyByTestAttr(wrapper, "component-app");
+
+  expect(appComponent).toHaveLength(1);
 });
